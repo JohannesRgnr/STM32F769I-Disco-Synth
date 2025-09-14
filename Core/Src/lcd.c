9ff343d58244c79710966cc205396ddf8438d695
JohannesRgnr <johannes.regnier@proton.me>
2025-09-14 14:03:04 +0200

    correct font

--- Core/Src/lcd.c
+++ Core/Src/lcd.c
@@ -36,30 +36,30 @@ void Display_Init(void)
 
 
   /* Display LCD X-Y Pads */
-	BSP_LCD_SetTextColor(LCD_COLOR_DARKGRAY);
-	BSP_LCD_DrawRect(16, 16, BSP_LCD_GetXSize() - 32, BSP_LCD_GetYSize()/2 - 48);
+	// BSP_LCD_SetTextColor(LCD_COLOR_DARKGRAY);
+	// BSP_LCD_DrawRect(16, 16, BSP_LCD_GetXSize() - 32, BSP_LCD_GetYSize()/2 - 48);
   // BSP_LCD_FillRect(BSP_LCD_GetXSize()/2 +1, 26, BSP_LCD_GetXSize()/2 - 24, BSP_LCD_GetYSize()/2 - 24);
   // BSP_LCD_SetTextColor(LCD_COLOR_LIGHTBLUE);
   // BSP_LCD_FillRect(25, 25, BSP_LCD_GetXSize()/2 - 49, BSP_LCD_GetYSize() - 49);
   // BSP_LCD_FillRect(BSP_LCD_GetXSize()/2, 25, BSP_LCD_GetXSize()/2 , BSP_LCD_GetYSize()/2 - 25);
 
 	/* Set the LCD Text Color */
-	BSP_LCD_SetTextColor(LCD_COLOR_DARKGRAY);
+	BSP_LCD_SetTextColor(LCD_COLOR_LIGHTGRAY);
 	BSP_LCD_SetBackColor(LCD_COLOR_BLACK);
 	/* Display LCD messages */
-	BSP_LCD_SetFont(&FontInconsolataNerdFont32);
-	BSP_LCD_DisplayStringAt(640, 20, (uint8_t *)"partials", LEFT_MODE);
+	BSP_LCD_SetFont(&FontInconsolataNerdFont20);
+	BSP_LCD_DisplayStringAt(16, 20, (uint8_t *)"SAI DEMO - 2*50 voices additive synthesis", CENTER_MODE);
 	// BSP_LCD_DisplayStringAt(2, BSP_LCD_GetYSize() - 16, (uint8_t *)"Getysize - 16", LEFT_MODE);
 
 
-	BSP_LCD_SetTextColor(LCD_COLOR_GRAY);
-	BSP_LCD_DrawHLine(32, BSP_LCD_GetYSize()/2 - 48, BSP_LCD_GetXSize() - 64);
-	BSP_LCD_DrawVLine(32, 32, BSP_LCD_GetYSize()/2 - 80);
+//	BSP_LCD_SetTextColor(LCD_COLOR_GRAY);
+//	BSP_LCD_DrawHLine(32, BSP_LCD_GetYSize()/2 - 48, BSP_LCD_GetXSize() - 64);
+//	BSP_LCD_DrawVLine(32, 32, BSP_LCD_GetYSize()/2 - 80);
   // Point Points[]= {{BSP_LCD_GetXSize() - 33, 220}, {BSP_LCD_GetXSize() - 33, 240}, {BSP_LCD_GetXSize() - 16, 230}};
   // BSP_LCD_FillPolygon(Points, 3);
 
   // Display_Default();
-	Display_partials();
+//	Display_partials();
 }
 
 void Display_partials(void)
